32c1cd97d385154e6b60f6c12d1fd79bc6c0e542
Victor Hugo Carvalho Dos Reis Santos <admin@katomaro.com>
2025-11-20 18:21:55 -0300

    version

--- src/config/version.py
+++ src/config/version.py
@@ -1,2 +1,2 @@
 BUILD_NUMBER = 2
-VERSION_FILE_URL = "https://raw.githubusercontent.com/katomaro/katomart/main/VERSION.json"
+VERSION_FILE_URL = "https://raw.githubusercontent.com/katomaro/katomart/refs/heads/master/VERSION.json"
